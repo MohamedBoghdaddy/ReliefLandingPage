baa0575cbaa4649dfcb6b10dbccedbb78fdee852
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-08-30 19:41:56 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,12 +40,12 @@ app.get("/test", (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Handle React routing, return all requests to React app
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
-);
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
+// );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
